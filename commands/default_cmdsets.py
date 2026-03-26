f7dbe1e7ecd0512b4562aff07ecad2c1eb941e39
CKWich <ckwich@gmail.com>
2026-03-25 22:53:23 -0700

    feat(05-04): wire guild discovery hook and register all commands
    - _check_guild_discovery() fires after commit_session_xp with try/except safety wrap
    - Guild recruitment message sent when any domain reaches 30+ (D-17)
    - All 5 new commands registered in CharacterCmdSet (ancestry, guild, domains, abilities, use)

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -34,6 +34,15 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         #
         # any commands you add below will overload the default ones.
         #
+        from commands.cmd_ancestry import CmdSetAncestry
+        self.add(CmdSetAncestry())
+        from commands.cmd_guild import CmdJoinGuild
+        self.add(CmdJoinGuild())
+        from commands.cmd_domains import CmdDomains
+        self.add(CmdDomains())
+        from commands.cmd_abilities import CmdAbilities, CmdUseAbility
+        self.add(CmdAbilities())
+        self.add(CmdUseAbility())
 
 
 class AccountCmdSet(default_cmds.AccountCmdSet):
